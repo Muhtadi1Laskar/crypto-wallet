533492438e875ea44e7746d24b2ea2a633c473bc
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-17 19:23:56 +0600

    Added the phrase generate handler

---
+++ handlers/key_handlers.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type KeyRequestBody struct {
+	Name string `json:"name" validate:"required"`
+	Email string `json:"email" validate:"required"`
+}
+
+type KeyResponse struct {
+	Phrase string `json:"phrase"`
+}
+
+func GeneratePhrase(w http.ResponseWriter, r *http.Request) {
+	var requestBody KeyRequestBody
+
+	if err := readRequestBody(r, &requestBody); err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	fmt.Println(requestBody.Name)
+	fmt.Println(requestBody.Name)
+}
--- main.go
+++ main.go
@@ -2,7 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
+
+func enableCORS(h http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+        if r.Method == "OPTIONS" {
+            w.WriteHeader(http.StatusOK)
+            return
+        }
+
+        h.ServeHTTP(w, r)
+    })
+}
 
 func main() {
 	fmt.Println("Hello World")
