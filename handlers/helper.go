c9452dc938c756a5ec572fa52de45cce2fd32d2c
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-17 19:18:06 +0600

    Added the helper function

---
+++ handlers/helper.go
@@ -0,0 +1,61 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func EnableCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Add CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Handle preflight requests
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Pass to the next handler
+		next.ServeHTTP(w, r)
+	})
+}
+
+func readRequestBody(r *http.Request, target interface{}) error {
+	reqBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		return fmt.Errorf("unable to read request body: %v", err)
+	}
+	defer r.Body.Close()
+
+	if err := json.Unmarshal(reqBody, target); err != nil {
+		return fmt.Errorf("unable to read the request body: %v", err)
+	}
+
+	validator := validator.New()
+	err = validator.Struct(target)
+	if err != nil {
+		return fmt.Errorf("validation error: %w", err)
+	}
+	return nil
+}
+
+func writeJSONResponse(w http.ResponseWriter, statusCode int, response interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(response)
+}
+
+func writeError(w http.ResponseWriter, statuCode int, err string) {
+	writeJSONResponse(w, statuCode, ErrorResponse{ Error: err })
+}
